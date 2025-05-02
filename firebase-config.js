c02097f2f2623be102a59f52dc87aa60bfb9fc29
Smultronet <karim.nawar.stockholm@gmail.com>
2025-05-02 09:38:56 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBAVCGzNcSEIuiEGqUJtUyv_-k5tY80WCs",
   authDomain: "smultronet-project.firebaseapp.com",
   projectId: "smultronet-project",
-  storageBucket: "smultronet-project.firebasestorage.app",
+  storageBucket: "smultronet-project.appspot.com",
   messagingSenderId: "1052103798715",
   appId: "1:1052103798715:web:8f61c491a65a4cd09a9e9e"
 };
