9add396045971530d5f6a5b916cb9b83e6770c1f
Smultronet <karim.nawar.stockholm@gmail.com>
2025-05-15 13:58:08 +0200

    Update email.js

--- email.js
+++ email.js
@@ -1,21 +1,18 @@
 // email.js
 async function skickaBekraftelse(parentName, parentEmail, youthName, summary, kommentar) {
-  const common = {
+  const data = {
     from_email:   "no-reply@stockholm.se",
+    to_email:     parentEmail,
     cc_emails:    "karim.nawar@stockholm.se, tina.han.yeung@stockholm.se, marie.faniadis@stockholm.se",
     parent_name:  parentName,
     parent_email: parentEmail,
     child_name:   youthName,
-    summary,
-    kommentar     // skickar med kommentaren
+    summary:      summary,
+    kommentar:    kommentar
   };
 
   try {
-    // Till förälder + CC
-    await emailjs.send("service_1n11tf8", "template_o3xaajo", {
-      ...common,
-      to_email: parentEmail
-    });
+    await emailjs.send("service_1n11tf8", "template_o3xaajo", data);
     return true;
   } catch (err) {
     console.error("Fel vid e-postutskick:", err);
